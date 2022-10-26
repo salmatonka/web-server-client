15405d526902a019d8f9e2559018c77b9fa634b4
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-10-27 01:16:30 +0600

    forth commit

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ const port = process.env.PORT ||5000
 app.use(cors());
 const courses = require('./data/courses.json');
 
-app.get('/details-courses', (req, res) => {
+
+app.get('/courses', (req, res) => {
   res.send(courses)
 })
 
+
 app.get('/',(req,res)=>{
-    res.send('news api')
+    res.send('courses api')
 })
+
+
+
+
+
 
 app.listen(port, () => {
     console.log('courses server',port)
