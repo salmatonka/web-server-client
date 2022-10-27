72cd938ff8680e78becf76914cd857ba6075b524
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-10-27 21:00:24 +0600

    sixth commit

--- index.js
+++ index.js
@@ -5,21 +5,43 @@ const cors =require('cors');
 const port = process.env.PORT ||5000
 
 app.use(cors());
-const courses = require('./data/courses.json');
+const items = require('./data/items.json');
+const details = require('./data/details.json');
 
 
-app.get('/courses', (req, res) => {
-  res.send(courses)
+app.get('/items',(req,res)=>{
+ 
+  res.send(items)
+
 })
+app.get('/details',(req,res)=>{
+ 
+  res.send(details)
+
+})
+app.get('/details/:id',(req,res)=>{
+   const id =req.params.id;
+   const selectDetails =details.find(d=d._id ===id);
+  res.send(selectDetails)
+
+})
+
+
+
+
+app.get('/item/:id',(req,res)=>{
+  const id = req.params.id;
+
+const selectedItem = details.find( d=>d._id ===id)
+res.send(selectedItem)
+  
+})
+
 
 
 app.get('/',(req,res)=>{
     res.send('courses api')
 })
-
-
-
-
 
 
 app.listen(port, () => {
